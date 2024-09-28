85dfece163217325243f4e8c35954d909dd790ee
Saud Alkatheeri <salkatheeri1@icloud.com>
2024-09-29 00:13:30 +0300

    Login and client registration frontend-backend integrated, business registration backend function added

--- wsl-app/backend/models/business-model.js
+++ wsl-app/backend/models/business-model.js
@@ -6,10 +6,12 @@ const businessSchema = new mongoose.Schema({
   businessName: { type: String, required: true },
   email: { type: String, required: true },
   phoneNumber: { type: String, required: true }, // Business owner's phone number
-  location: { type: String, required: true },
-  categories: [{ type: String, required: true, enum: ['Electricians', 'Car Washes', 'Plumbers', 'Mechanics', 'Cleaning Services']}], // Array of categories (e.g., electricians, car washes)
-  description: { type: String, required: true }, // Business description
-  images: [{ type: String }],
+  location:  {
+    city: { type: String, required: true },
+    street: { type: String, required: true },
+  },
+  categories: [{ type: String, required: true, }], // Array of categories (e.g., electricians, car washes)
+  description: { type: String, required: true }, // Business description enum: ['Electricians', 'Car Washes', 'Plumbers', 'Mechanics', 'Cleaning Services']
   openingHours: { type: String, required: true },
   ratings: { type: Number, default: 0 }, // Average rating for the business
   reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }], // Array of review references
--- wsl-app/backend/routes/auth-route.js
+++ wsl-app/backend/routes/auth-route.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { registerUser, login, logout} from "../controllers/auth-cont.js";
+import { registerBusiness,registerUser, login, logout} from "../controllers/auth-cont.js";
 const router = express.Router();
 
+
+router.post("/registerbusiness", registerBusiness);
 router.post("/register",registerUser);
 router.post("/login", login);
 router.post("/logout",logout);
