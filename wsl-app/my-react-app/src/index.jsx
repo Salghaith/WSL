1c63cbc4f905cad227e902ba58fbc9d8dabf97f3
Salghaith <146896271+Salghaith@users.noreply.github.com>
2024-11-03 22:24:59 +0300

    handling security issues

--- wsl-app/my-react-app/src/index.jsx
+++ wsl-app/my-react-app/src/index.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+
+const script = document.createElement("script");
+script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+script.async = true;
+script.defer = true;
+
+document.head.appendChild(script);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
